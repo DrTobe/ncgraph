c418ba0a1f36577a2e5a85303eadfa7ae0cc5789
Harry Beadle <harry.beadle@gmail.com>
2018-01-16 00:29:20 +0000

    +color, +legends

--- ncgraph.py
+++ ncgraph.py
@@ -5,24 +5,32 @@ def DEBUG(string):
     debugfile.write("%s\n" % string)
 
 class DataSeries(object):
-    def __init__(self, X, Y, label):
+    def __init__(self, X, Y, label, color=0):
         assert (len(X) == len(Y))
         self.X = X.copy()
         self.Y = Y.copy()
         self.label = label
         self.length = len(X)
+        self.color = color
 
 class Grapher(object):
     seriesList = []
     legend = False
+    colorList = [2, 3, 4, 5, 6, 7]
+    autoAxis = True
 
     def __init__(self, window):
+        # Setup the window
         self.setWindow(window)
+        # Initalise color
+        curses.start_color()
+        curses.use_default_colors()
+        for i in range(curses.COLORS):
+            curses.init_pair(i+1, i, -1)
 
     def setWindow(self, window):
-        self.window = window
         self.plotArea = window # TODO: Setup boarders with plotArea
-        self.height, self.width = self.window.getmaxyx()
+        self.height, self.width = self.plotArea.getmaxyx()
 
     def setAxis(self, x_min=0, x_max=0, y_min=0, y_max=0, auto=False):
         self.autoAxis = auto
@@ -45,14 +53,8 @@ class Grapher(object):
                 seriesYMaxs.append(max(ds.Y))
             self.x_min = min(seriesXMins)
             self.x_max = max(seriesXMaxs)
-            self.y_min = min(seriesYMins)
-            self.y_max = max(seriesYMaxs)
-            DEBUG("NOTE x_min:%f" % self.x_min)
-            DEBUG("NOTE x_max:%f" % self.x_max)
-            DEBUG("NOTE y_min:%f" % self.y_min)
-            DEBUG("NOTE y_max:%f" % self.y_max)
-            for i in seriesXMaxs:
-                DEBUG("NOTE SXM %f" % (i))
+            self.y_min = min(seriesYMins) - 0.1 * (max(seriesYMaxs) - min(seriesYMins))
+            self.y_max = max(seriesYMaxs) + 0.1 * (max(seriesYMaxs) - min(seriesYMins))
         self.plotAxis()
 
     def plotAxis(self):
@@ -74,7 +76,15 @@ class Grapher(object):
         self.updateLegend();
 
     def updateLegend(self):
-        pass
+        y = 0
+        labelLengths = []
+        for ds in self.seriesList:
+            labelLengths.append(len(ds.label))
+        x = self.width - max(labelLengths)
+        for ds in self.seriesList:
+            self.plotArea.addstr(y, x, " "*max(labelLengths), curses.color_pair(ds.color) | curses.A_REVERSE)
+            self.plotArea.addstr(y, x, ds.label, curses.color_pair(ds.color) | curses.A_REVERSE)
+            y += 1
 
     def mapValue(self, value, origin_start, origin_end, output_start, output_end):
         origin_proportion = (value - origin_start) / (origin_end - origin_start)
@@ -107,7 +117,7 @@ class Grapher(object):
                     px = self.mapValue(x, self.x_min, self.x_max, 0, self.width-1)
                     py = self.height-1 - self.mapValue(y, self.y_min, self.y_max, 0, self.height-1)
                     # Plot the point at that location.
-                    self.plotArea.addstr(py, px, "#")
+                    self.plotArea.addstr(py, px, " ", curses.color_pair(ds.color) | curses.A_REVERSE)
 
     def clearPlotArea(self):
         self.plotArea.addstr(0, 0, " ")
@@ -118,7 +128,7 @@ class Grapher(object):
 
     def plot(self, X, Y, label="myData"):
         # Add the new data to the seriesList.
-        self.seriesList.append(DataSeries(X, Y, label))
+        self.seriesList.append(DataSeries(X, Y, label, color=self.colorList[len(self.seriesList)]))
         # Clear the screen of any current plots.
         self.clearPlotArea()
         # Update and plot the axis.
@@ -136,19 +146,14 @@ curses.noecho()
 curses.cbreak()
 
 x, y = [], []
-for i in numpy.arange(0, 360, 0.25):
-    j = math.cos((i/180) * math.pi)
-    x.append(i)
-    y.append(j)
-
+x = [i for i in numpy.arange(-3.5, 3.5, 0.01)]
+ya = [math.sin(i) for i in x]
+yb = [(1/4)*math.sin(4*i) for i in x]
+yc = [math.sin(i) + (1/4)*math.sin(4*i) for i in x]
 myGrapher = Grapher(stdscr)
-myGrapher.setAxis(auto=True)
-myGrapher.plot(x, y, label="cos(x) for 0<x<360")
-
-stdscr.getch()
-
-for i in range(len(x)): x[i] = (-x[i])
-myGrapher.plot(x, y, label="cos(-x) for 0<x<360")
+myGrapher.plot(x, ya, label="sin(x)")
+myGrapher.plot(x, yb, label="(1/4)sin(2x)")
+myGrapher.plot(x, yc, label="sin(x)+(1/4)sin(2x)")
 
 stdscr.getch()
 
